cb8726e8f71f23ddee474a24d0d67bb48350712a
Nehal Gupta <61117568+nehalgupta8501@users.noreply.github.com>
2020-07-21 17:23:27 +0530

    Add files via upload
    Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
    Return the running sum of nums.

---
+++ RunningArraySum.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums) {
+        int n=nums.size();
+        for(int i=0; i<n; i++)
+        {
+           if(i>0)
+           {
+               nums[i]+=nums[i-1];
+           }
+        }
+        return nums;
+    }
+};
