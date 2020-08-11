bffd6ef08104951e5bb1363ad7efe9523fe49862
Nehal Gupta <61117568+nehalgupta8501@users.noreply.github.com>
2020-08-11 19:14:22 +0530

    Add files via upload

---
+++ DuplicateArray.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> findDuplicates(vector<int>& nums) {
+        vector<int> ans;
+        if(nums.empty())return {};
+        for(int i=0; i<nums.size(); i++)
+        {
+            if(nums[abs(nums[i])-1]>0)
+            {
+                nums[abs(nums[i])-1]=-nums[abs(nums[i])-1];
+            }
+            else
+            {
+                ans.push_back(abs(nums[i]));
+            }
+        }
+        return ans;
+    }
+};
