240ce7dfd44040b446a70f34553eb6b24e080e7e
Nehal Gupta <61117568+nehalgupta8501@users.noreply.github.com>
2020-08-23 19:15:13 +0530

    Add files via upload

---
+++ RemoveDuplicateLL.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        ListNode *temp=head;
+        while(temp!=NULL && temp->next!=NULL )
+        {
+            if(temp->next->val==temp->val)
+            {
+                temp->next=temp->next->next;
+            }
+            else
+            {
+                temp=temp->next;
+            }
+        }
+        return head;
+    }
+};
